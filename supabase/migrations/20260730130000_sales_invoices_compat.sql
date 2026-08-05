ae4e7d7635e2443755f08af0d9dc995cc6de25f8
agarehim777-tech <agarehim777@gmail.com>
2026-08-05 12:57:17 +0400

    Remove obsolete stock routine permission

--- supabase/migrations/20260730130000_sales_invoices_compat.sql
+++ supabase/migrations/20260730130000_sales_invoices_compat.sql
@@ -184,7 +184,6 @@ END $$;
 REVOKE ALL ON FUNCTION public.gl_account_by_code(UUID, TEXT) FROM PUBLIC, anon;
 REVOKE ALL ON FUNCTION public.post_invoice_to_gl(UUID) FROM PUBLIC, anon;
 REVOKE ALL ON FUNCTION public.post_payment_to_gl(UUID) FROM PUBLIC, anon;
-REVOKE ALL ON FUNCTION public.apply_stock_movement() FROM PUBLIC, anon, authenticated;
 REVOKE ALL ON FUNCTION public.sync_invoice_payment() FROM PUBLIC, anon, authenticated;
 GRANT EXECUTE ON FUNCTION public.post_invoice_to_gl(UUID) TO authenticated;
 GRANT EXECUTE ON FUNCTION public.post_payment_to_gl(UUID) TO authenticated;
